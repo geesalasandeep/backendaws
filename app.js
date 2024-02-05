3da149d3245200259525e1a3d05b6e5f2c45f9a7
sandeep <geesala.k@testyantra.com>
2024-02-05 17:52:26 +0530

    qwertyu

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
-app.listen(4200,(req,res,next)=>{
+
+app.get('/',(req,res,next)=>{
     res.send('its working....')
+})
+app.listen(4200,(req,res,next)=>{
+    console.log('qwert');
 })
